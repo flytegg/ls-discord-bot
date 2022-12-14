c34f6e58ad2d4fb783ca2f2c77588581b435134d
nucker <nuckermail@gmail.com>
2022-12-14 19:16:49 +0000

    Update data file defaults

--- src/main/kotlin/com/learnspigot/bot/entity/DataFile.kt
+++ src/main/kotlin/com/learnspigot/bot/entity/DataFile.kt
@@ -4,8 +4,8 @@ import org.spongepowered.configurate.objectmapping.ConfigSerializable
 
 @ConfigSerializable
 data class DataFile(
-    var migrated: Boolean = false,
-    var migratedRep: Boolean = false,
+    var migrated: Boolean = true,
+    var migratedRep: Boolean = true,
     val bearer: String = System.getenv("UDEMY_BEARER"),
     var allTimeLeaderboards: List<String> = emptyList(),
     var monthlyLeaderboards: List<String> = emptyList(),

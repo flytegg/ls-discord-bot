d602888373ce18aff71161ace7d35a88b08b8120
Stephen <hello@stephen.gg>
2023-06-02 01:09:45 +0100

    Update Embed.kt
    Update embed color (Thanks, Discord)

--- src/main/kotlin/com/learnspigot/bot/util/Embed.kt
+++ src/main/kotlin/com/learnspigot/bot/util/Embed.kt
@@ -3,5 +3,5 @@ package com.learnspigot.bot.util
 import net.dv8tion.jda.api.EmbedBuilder
 
 fun embed(): EmbedBuilder {
-    return EmbedBuilder().setColor(0x2F3136)
+    return EmbedBuilder().setColor(0x2B2D31)
 }

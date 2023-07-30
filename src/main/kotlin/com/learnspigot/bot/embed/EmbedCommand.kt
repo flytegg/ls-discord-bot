eeab0ecb785e95b002e13f71cbbebadcc15aa3e9
akkkih <media@akkih.com>
2023-07-29 22:57:42 -0300

    Add permission requirement for embed command

--- src/main/kotlin/com/learnspigot/bot/embed/EmbedCommand.kt
+++ src/main/kotlin/com/learnspigot/bot/embed/EmbedCommand.kt
@@ -5,13 +5,15 @@ import com.learnspigot.bot.util.embed
 import gg.flyte.neptune.annotation.Command
 import gg.flyte.neptune.annotation.Description
 import gg.flyte.neptune.annotation.Optional
+import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.channel.Channel
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 
 class EmbedCommand {
     @Command(
         name = "embed",
-        description = "Create an embed message"
+        description = "Create an embed message",
+        permissions = [Permission.MESSAGE_MANAGE]
     )
     fun onEmbedCommand(
         event: SlashCommandInteractionEvent,

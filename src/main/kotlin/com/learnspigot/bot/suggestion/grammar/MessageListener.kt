6fbd5c123d1044fdfc811739a1e3d5edc3539321
Dawson <hello@dawson.gg>
2023-10-06 21:48:13 -0400

    L emoji added to bot message

--- src/main/kotlin/com/learnspigot/bot/suggestion/grammar/MessageListener.kt
+++ src/main/kotlin/com/learnspigot/bot/suggestion/grammar/MessageListener.kt
@@ -1,13 +1,14 @@
 package com.learnspigot.bot.suggestion.grammar
 
+import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 class MessageListener : ListenerAdapter() {
 
     private val idiotGrammerFixerBroLikeActuallyStfuOngUCantSpellDumbAss = hashSetOf(
-        "I m",
-        "I ma",
+        "I m ",
+        "I ma ",
     )
 
     private val theIdiot = "1071963283332018177"
@@ -15,7 +16,10 @@ class MessageListener : ListenerAdapter() {
     override fun onMessageReceived(event: MessageReceivedEvent) {
         if (event.author.id == theIdiot) {
             if (idiotGrammerFixerBroLikeActuallyStfuOngUCantSpellDumbAss.any { event.message.contentRaw.contains(it, true) }) {
-                event.channel.sendMessage("<@${theIdiot}> I'm*").queue()
+                event.channel.sendMessage("<@${theIdiot}> I'm*").queue() {
+                    it.addReaction(Emoji.fromFormatted(":regional_indicator_l:")).queue()
+                }
+
                 event.message.delete().queue()
             }
         }

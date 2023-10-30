f2c582a6dc18fd2e6659564d11df474a232ae2af
Josh <jshbker@gmail.com>
2023-10-30 15:00:08 +0000

    feat: clean up old messages in verify channel before sending new messages

--- src/main/kotlin/com/learnspigot/bot/verification/VerificationMessage.kt
+++ src/main/kotlin/com/learnspigot/bot/verification/VerificationMessage.kt
@@ -1,20 +1,30 @@
 package com.learnspigot.bot.verification
 
 import com.learnspigot.bot.Environment
+import com.learnspigot.bot.Server
 import com.learnspigot.bot.util.embed
 import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.MessageHistory
 import net.dv8tion.jda.api.interactions.components.buttons.Button
 
 class VerificationMessage(guild: Guild) {
 
     init {
-        guild.getTextChannelById(Environment.get("VERIFY_CHANNEL_ID"))!!.sendMessageEmbeds(
+        // Clean up old message(s)
+        MessageHistory.getHistoryFromBeginning(Server.verifyChannel)
+            .complete().retrievedHistory.forEach { message -> message.delete().queue() }
+
+        // Send new message
+        Server.verifyChannel.sendMessageEmbeds(
             embed()
                 .setTitle("VERIFY YOU OWN THE COURSE")
-                .setDescription("""
+                .setDescription(
+                    """
                                 Welcome to the Discord for the LearnSpigot course!
                                                                 
-                                :disappointed: **Don't own the course? See """.trimIndent() + guild.getTextChannelById(Environment.get("GET_COURSE_CHANNEL_ID"))!!.asMention + """
+                                :disappointed: **Don't own the course? See """.trimIndent() + guild.getTextChannelById(
+                        Environment.get("GET_COURSE_CHANNEL_ID")
+                    )!!.asMention + """
                                 **
                                                         
                                 The URL you need to use is the link to your public profile, to get this:
@@ -22,11 +32,12 @@ class VerificationMessage(guild: Guild) {
                                 :two: Select "Public profile" from the dropdown menu
                                 :three: Copy the link from your browser
                                                                 
-                                Please make sure that you have [privacy settings](https://www.udemy.com/instructor/profile/privacy/) enabled so that we can verify you own the course.""".trimIndent())
+                                Please make sure that you have [privacy settings](https://www.udemy.com/instructor/profile/privacy/) enabled so that we can verify you own the course.""".trimIndent()
+                )
                 .setFooter("Once you've verified, you'll have access to our 50 man support team, hundreds of additional tutorials and a supportive community.")
-                .build())
+                .build()
+        )
             .addActionRow(Button.success("verify", "Click to Verify"))
             .queue()
     }
-
 }

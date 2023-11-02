f447ce0b946a8a88114963d9d6e9e0eb5ec684eb
Josh <jshbker@gmail.com>
2023-11-02 10:48:18 +0000

    fix(edit-startup-messages): can't call first if it's empty, check if history is empty

--- src/main/kotlin/com/learnspigot/bot/verification/VerificationMessage.kt
+++ src/main/kotlin/com/learnspigot/bot/verification/VerificationMessage.kt
@@ -10,20 +10,18 @@ import net.dv8tion.jda.api.interactions.components.buttons.Button
 class VerificationMessage(guild: Guild) {
 
     init {
-        val message = MessageHistory.getHistoryFromBeginning(Server.verifyChannel)
-            .complete().retrievedHistory
-            .first()
-
-        if (message == null) Server.verifyChannel.sendMessageEmbeds(
-            embed()
-                .setTitle("VERIFY YOU OWN THE COURSE")
-                .setDescription(
-                    """
+        val history = MessageHistory.getHistoryFromBeginning(Server.verifyChannel).complete().retrievedHistory
+        if (history.isEmpty())
+            Server.verifyChannel.sendMessageEmbeds(
+                embed()
+                    .setTitle("VERIFY YOU OWN THE COURSE")
+                    .setDescription(
+                        """
                                     Welcome to the Discord for the LearnSpigot course!
                                                                     
                                     :disappointed: **Don't own the course? See """.trimIndent() + guild.getTextChannelById(
-                        Environment.get("GET_COURSE_CHANNEL_ID")
-                    )!!.asMention + """
+                            Environment.get("GET_COURSE_CHANNEL_ID")
+                        )!!.asMention + """
                                     **
                                                             
                                     The URL you need to use is the link to your public profile, to get this:
@@ -32,12 +30,12 @@ class VerificationMessage(guild: Guild) {
                                     :three: Copy the link from your browser
                                                                     
                                     Please make sure that you have [privacy settings](https://www.udemy.com/instructor/profile/privacy/) enabled so that we can verify you own the course.""".trimIndent()
-                )
-                .setFooter("Once you've verified, you'll have access to our 50 man support team, hundreds of additional tutorials and a supportive community.")
-                .build()
-        )
-            .addActionRow(Button.success("verify", "Click to Verify"))
-            .queue()
+                    )
+                    .setFooter("Once you've verified, you'll have access to our 50 man support team, hundreds of additional tutorials and a supportive community.")
+                    .build()
+            )
+                .addActionRow(Button.success("verify", "Click to Verify"))
+                .queue()
     }
 
 }

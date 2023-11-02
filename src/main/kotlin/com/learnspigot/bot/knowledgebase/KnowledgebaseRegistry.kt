6547ef0060981ef98111177535bea98d1c5b856b
Josh <jshbker@gmail.com>
2023-11-02 07:45:45 +0000

    feat(knowledgebase-search): Extract KnowledgebaseRegistry into a generic PostRegistry
    Done in prep for HelpPostRegistry. A class implementing PostRegistry can determine what content is in the registry

--- src/main/kotlin/com/learnspigot/bot/knowledgebase/KnowledgebaseRegistry.kt
+++ src/main/kotlin/com/learnspigot/bot/knowledgebase/KnowledgebaseRegistry.kt
@@ -2,23 +2,15 @@ package com.learnspigot.bot.knowledgebase
 
 import com.learnspigot.bot.Server
 import com.learnspigot.bot.lecture.WordMatcher
+import com.learnspigot.bot.util.PostRegistry
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel
 
-class KnowledgebaseRegistry {
-
-    val posts = mutableMapOf<String, String>() // name, id
-    private val matcher: WordMatcher = WordMatcher()
+class KnowledgebaseRegistry : PostRegistry() {
 
     init {
         Server.knowledgebaseChannel.threadChannels.forEach {
             posts[it.name] = it.id
         }
-    }
-
-    fun findTop4Posts(query: String): MutableList<ThreadChannel> {
-        return matcher.getTopMatches(query, posts.keys.toList(), 4).mapNotNull {
-            Server.guild.getThreadChannelById(posts[it]!!)
-        }.toMutableList()
     }
 
 }
---
+++ src/main/kotlin/com/learnspigot/bot/util/PostRegistry.kt
@@ -0,0 +1,18 @@
+package com.learnspigot.bot.util
+
+import com.learnspigot.bot.Server
+import com.learnspigot.bot.lecture.WordMatcher
+import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel
+
+open class PostRegistry {
+
+    val posts = mutableMapOf<String, String>() // name, id
+    private val matcher: WordMatcher = WordMatcher()
+
+    fun findTop4Posts(query: String): MutableList<ThreadChannel> {
+        return matcher.getTopMatches(query, posts.keys.toList(), 4).mapNotNull {
+            Server.guild.getThreadChannelById(posts[it]!!)
+        }.toMutableList()
+    }
+
+}

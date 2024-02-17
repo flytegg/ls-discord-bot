0a3f69467aceeb3916de67390266269a39a2d519
Josh <43449531+joshbker@users.noreply.github.com>
2024-02-17 20:05:36 +0000

    feat: create vc and reply with mention, move if they are already in a vc, delete the vc after 5 minutes if it's empty

--- src/main/kotlin/com/learnspigot/bot/voicechat/VCCommand.kt
+++ src/main/kotlin/com/learnspigot/bot/voicechat/VCCommand.kt
@@ -6,30 +6,42 @@ import gg.flyte.neptune.annotation.Description
 import gg.flyte.neptune.annotation.Optional
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
 
 class VCCommand {
 
-  @Command(
-      name = "createvoice",
-      description = "Create a temporary voice channel!",
-      permissions = [Permission.CREATE_PUBLIC_THREADS])
-  fun onCreateVoiceCommand(
-      event: SlashCommandInteractionEvent,
-      @Description("Max user limit") @Optional limit: Int?,
-  ) {
-      if (event.member!!.voiceState?.inAudioChannel() != true){
-          event.reply("You must be in a voice channel to use this command").setEphemeral(true).queue()
-          return
-      }
+    private val scheduledExecutor: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
 
-      val guild = event.guild ?: return
-      val newChannel = guild.createVoiceChannel("${event.member!!.effectiveName}'s channel", event.guild!!.getCategoryById(Environment.get("CHAT_CATEGORY"))).complete()
+    @Command(
+        name = "createvoice",
+        description = "Create a temporary voice channel!",
+        permissions = [Permission.CREATE_PUBLIC_THREADS]
+    )
+    fun onCreateVoiceCommand(
+        event: SlashCommandInteractionEvent,
+        @Description("Max user limit") @Optional limit: Int?,
+    ) {
+        val guild = event.guild ?: return
+        val newChannel = guild.createVoiceChannel(
+            "${event.member!!.effectiveName}'s channel",
+            event.guild!!.getCategoryById(Environment.get("CHAT_CATEGORY"))
+        ).complete()
 
-      if (limit != null) {
-          newChannel.manager.setUserLimit(limit).queue()
-      }
+        if (limit != null) {
+            newChannel.manager.setUserLimit(limit).queue()
+        }
 
-      guild.moveVoiceMember(event.member!!, newChannel).queue()
-      event.reply("VC created, enjoy!").setEphemeral(true).queue()
-  }
+        if (event.member!!.voiceState?.inAudioChannel() == true)
+            guild.moveVoiceMember(event.member!!, newChannel).queue()
+
+        event.reply("Your voice channel has been created - ${newChannel.asMention}").setEphemeral(true).queue()
+
+        scheduledExecutor.schedule({
+            if (newChannel == null) return@schedule
+            if (newChannel.members.isEmpty()) newChannel.delete().queue()
+        }, 5, TimeUnit.MINUTES)
+    }
+
 }

759b374d42a866ec5cf06750d787bf8b3c9e88fc
Dawson <hello@dawson.gg>
2024-07-22 10:45:02 -0400

    Enable use of system properties for env vars

--- src/main/kotlin/com/learnspigot/bot/Environment.kt
+++ src/main/kotlin/com/learnspigot/bot/Environment.kt
@@ -3,7 +3,9 @@ package com.learnspigot.bot
 import io.github.cdimascio.dotenv.Dotenv
 
 object Environment {
-    private val dotenv = Dotenv.load()
+    private val dotenv = Dotenv.configure()
+        .systemProperties()
+        .load()
 
     fun get(variable: String): String {
         return dotenv.get(variable)

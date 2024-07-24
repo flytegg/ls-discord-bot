338094d850a79380eb61710274c2cb291cdac968
Dawson <hello@dawson.gg>
2024-07-24 09:33:07 -0400

    Profile registry recode finished (maybe)

--- src/main/kotlin/com/learnspigot/bot/profile/ProfileListener.kt
+++ src/main/kotlin/com/learnspigot/bot/profile/ProfileListener.kt
@@ -10,55 +10,54 @@ import net.dv8tion.jda.api.exceptions.ErrorHandler
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.requests.ErrorResponse
 
-class ProfileListener : ListenerAdapter() {
-
-    override fun onGuildMemberJoin(e: GuildMemberJoinEvent) {
-        if (e.guild.id != Server.guildId) return
-
-        val document = Mongo.userCollection.find(Filters.eq("_id", e.user.id)).first()
-        if (document != null && document.containsKey("udemyProfileUrl")) {
-            e.user.openPrivateChannel().complete().let {
-                it.sendMessageEmbeds(
-                    embed()
-                        .setTitle("Welcome to the Discord! :tada:")
-                        .setDescription(
-                            """
-                                You have already verified previously so your Student role has been restored.
-                                                           
-                                *PS: Use [our pastebin](https://paste.learnspigot.com) - pastes never expire!*
-                                
-                                """.trimIndent()
-                        )
-                        .build()
-                ).queue(null, ErrorHandler().handle(ErrorResponse.CANNOT_SEND_TO_USER) {})
-            }
-
-            e.guild.addRoleToMember(e.user, e.guild.getRoleById(Environment.STUDENT_ROLE_ID)!!).queue()
-        } else {
-            e.user.openPrivateChannel().complete().let {
-                it.sendMessageEmbeds(
-                    embed()
-                        .setTitle("Welcome to the Discord! :tada:")
-                        .setDescription(
-                            """
-                                You have joined the exclusive support community for the [Develop Minecraft Plugins (Java)](https://learnspigot.com) Udemy course.
-                                                                
-                                :question: Don't have the course? Grab it at <https://learnspigot.com>
-                                
-                                :thinking: Not convinced? Take a look at what everyone else has to say at <https://vouches.learnspigot.com>
-                                                                
-                                :star: Have it? Follow the instructions in """.trimIndent() + e.guild.getTextChannelById(Environment.get("VERIFY_CHANNEL_ID"))!!.asMention + """
-                                                       
-                                                           
-                                *PS: Use [our pastebin](https://paste.learnspigot.com) - pastes never expire!*
-                                
-                                """.trimIndent()
-                        )
-                        .setFooter("Without verifying, you can still read the server but won't have access to our 24/7 support team and dozens of tutorials and projects.")
-                        .build()
-                ).queue(null, ErrorHandler().handle(ErrorResponse.CANNOT_SEND_TO_USER) {})
-            }
-        }
-    }
-
-}
+//class ProfileListener : ListenerAdapter() {
+//
+//    override fun onGuildMemberJoin(e: GuildMemberJoinEvent) {
+//        if (e.guild.id != Server.guildId) return
+//
+//        val document = Mongo.userCollection.find(Filters.eq("_id", e.user.id)).first()
+//        if (document != null && document.containsKey("udemyProfileUrl")) {
+//            e.user.openPrivateChannel().complete().let {
+//                it.sendMessageEmbeds(
+//                    embed()
+//                        .setTitle("Welcome to the Discord! :tada:")
+//                        .setDescription(
+//                            """
+//                                You have already verified previously so your Student role has been restored.
+//
+//                                *PS: Use [our pastebin](https://paste.learnspigot.com) - pastes never expire!*
+//
+//                                """.trimIndent()
+//                        )
+//                        .build()
+//                ).queue(null, ErrorHandler().handle(ErrorResponse.CANNOT_SEND_TO_USER) {})
+//            }
+//
+//            e.guild.addRoleToMember(e.user, e.guild.getRoleById(Environment.STUDENT_ROLE_ID)!!).queue()
+//        } else {
+//            e.user.openPrivateChannel().complete().let {
+//                it.sendMessageEmbeds(
+//                    embed()
+//                        .setTitle("Welcome to the Discord! :tada:")
+//                        .setDescription(
+//                            """
+//                                You have joined the exclusive support community for the [Develop Minecraft Plugins (Java)](https://learnspigot.com) Udemy course.
+//
+//                                :question: Don't have the course? Grab it at <https://learnspigot.com>
+//
+//                                :thinking: Not convinced? Take a look at what everyone else has to say at <https://vouches.learnspigot.com>
+//
+//                                :star: Have it? Follow the instructions in """.trimIndent() + e.guild.getTextChannelById(Environment.get("VERIFY_CHANNEL_ID"))!!.asMention + """
+//
+//
+//                                *PS: Use [our pastebin](https://paste.learnspigot.com) - pastes never expire!*
+//
+//                                """.trimIndent()
+//                        )
+//                        .setFooter("Without verifying, you can still read the server but won't have access to our 24/7 support team and dozens of tutorials and projects.")
+//                        .build()
+//                ).queue(null, ErrorHandler().handle(ErrorResponse.CANNOT_SEND_TO_USER) {})
+//            }
+//        }
+//    }
+//}
--- src/main/kotlin/com/learnspigot/bot/profile/ProfileRegistry.kt
+++ src/main/kotlin/com/learnspigot/bot/profile/ProfileRegistry.kt
@@ -6,79 +6,80 @@ import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.User
 import org.bson.Document
 import java.util.*
-
-class ProfileRegistry {
-
-    val profileCache: MutableMap<String, Profile> = TreeMap(String.CASE_INSENSITIVE_ORDER)
-    private val urlProfiles: MutableMap<String, Profile> = TreeMap()
-
-    val contributorSelectorCache: MutableMap<String, List<String>> = HashMap()
-    val messagesToRemove: MutableMap<String, Message> = HashMap()
-
-    init {
-        Mongo.userCollection.find().forEach { document ->
-            val reputation: NavigableMap<Int, Reputation> = TreeMap()
-            document.get("reputation", Document::class.java).forEach { id, rep ->
-                val repDocument = rep as Document
-                reputation[id.toInt()] = Reputation(
-                    convertToLongTimestamp(repDocument["timestamp"]!!),
-                    repDocument.getString("fromMemberId"),
-                    repDocument.getString("fromPostId"))
-            }
-
-            Profile(
-                document.getString("_id"),
-                document.getString("tag"),
-                document.getString("udemyProfileUrl"),
-                reputation,
-                document.getBoolean("notifyOnRep", true),
-                document.getBoolean("intellijKeyGiven", false),
-                document.getInteger("highestCount", 0),
-                document.getInteger("totalCounts", 0),
-                document.getInteger("countingFuckUps", 0)
-            ).let {
-                profileCache[it.id] = it
-                if (it.udemyProfileUrl != null)
-                    urlProfiles[it.udemyProfileUrl!!] = it
-            }
-        }
-    }
-
-    // I don't even care enough to sort this bug so have this function instead
-    // Basically at some point they've been saving as Ints and some points Longs. So now we must read both. .-.
-    private fun convertToLongTimestamp(timestamp: Any): Long {
-        return when (timestamp) {
-            is Int -> timestamp.toLong()
-            is Long -> timestamp.toLong()
-            is String -> timestamp.toLongOrNull() ?: throw IllegalArgumentException("Invalid timestamp format")
-            else -> throw IllegalArgumentException("Unsupported timestamp format")
-        }
-    }
-
-    fun findById(id: String): Profile? {
-        return profileCache[id]
-    }
-
-    fun findByUser(user: User): Profile {
-        return findById(user.id) ?: run {
-            Profile(
-                user.id,
-                user.name,
-                null,
-                TreeMap(),
-                true,
-                false,
-                0,
-                0,
-                0,
-            ).apply {
-                    profileCache[user.id] = this
-                    save()
-                }
-        }
-    }
-
-    fun findByURL(udemyURL: String): Profile? {
-        return urlProfiles[udemyURL]
-    }
-}
+//
+//class ProfileRegistry {
+//
+////    val profileCache: MutableMap<String, Profile> = TreeMap(String.CASE_INSENSITIVE_ORDER)
+////    private val urlProfiles: MutableMap<String, Profile> = TreeMap()
+////
+////    val contributorSelectorCache: MutableMap<String, List<String>> = HashMap()
+////    val messagesToRemove: MutableMap<String, Message> = HashMap()
+////
+////    init {
+////        Mongo.userCollection.find().forEach { document ->
+////            val reputation: NavigableMap<Int, Reputation> = TreeMap()
+////            document.get("reputation", Document::class.java).forEach { id, rep ->
+////                val repDocument = rep as Document
+////                reputation[id.toInt()] = Reputation(
+////                    convertToLongTimestamp(repDocument["timestamp"]!!),
+////                    repDocument.getString("fromMemberId"),
+////                    repDocument.getString("fromPostId"))
+////            }
+////
+////            Profile(
+////                document.getString("_id"),
+////                document.getString("tag"),
+////                document.getString("udemyProfileUrl"),
+////                reputation,
+////                document.getBoolean("notifyOnRep", true),
+////                document.getBoolean("intellijKeyGiven", false),
+////                document.getInteger("highestCount", 0),
+////                document.getInteger("totalCounts", 0),
+////                document.getInteger("countingFuckUps", 0)
+////            ).let {
+////                profileCache[it.id] = it
+////                if (it.udemyProfileUrl != null)
+////                    urlProfiles[it.udemyProfileUrl!!] = it
+////            }
+////        }
+//}
+//
+//// this happens by default with kmongo now
+////// I don't even care enough to sort this bug so have this function instead
+////// Basically at some point they've been saving as Ints and some points Longs. So now we must read both. .-.
+////private fun convertToLongTimestamp(timestamp: Any): Long {
+////    return when (timestamp) {
+////        is Int -> timestamp.toLong()
+////        is Long -> timestamp.toLong()
+////        is String -> timestamp.toLongOrNull() ?: throw IllegalArgumentException("Invalid timestamp format")
+////        else -> throw IllegalArgumentException("Unsupported timestamp format")
+////    }
+////}
+//
+////fun findById(id: String): Profile? {
+////    return profileCache[id]
+////}
+//
+////fun findByUser(user: User): Profile {
+////    return findById(user.id) ?: run {
+////        Profile(
+////            user.id,
+////            user.name,
+////            null,
+////            TreeMap(),
+////            true,
+////            false,
+////            0,
+////            0,
+////            0,
+////        ).apply {
+////            profileCache[user.id] = this
+////            save()
+////        }
+////    }
+////}
+//
+////fun findByURL(udemyURL: String): Profile? {
+////    return urlProfiles[udemyURL]
+////}
+////}

33f2639b6d7f19ee00a31656dd24a2c9c2a4d7a8
Dawson <hello@dawson.gg>
2024-07-24 09:36:17 -0400

    Remove variables which were used in the pevious registry class which 1. Should not be here, 2. are useless

--- src/main/kotlin/com/learnspigot/bot/database/profile/ProfileManager.kt
+++ src/main/kotlin/com/learnspigot/bot/database/profile/ProfileManager.kt
@@ -7,10 +7,6 @@ import java.util.*
 
 object ProfileManager {
     val profileCache: MutableMap<String, Profile> = TreeMap(String.CASE_INSENSITIVE_ORDER)
-    private val urlProfiles: MutableMap<String, Profile> = TreeMap()
-
-    val contributorSelectorCache: MutableMap<String, List<String>> = HashMap()
-    val messagesToRemove: MutableMap<String, Message> = HashMap()
 
     fun getProfile(id: String): Profile? = profileCache[id] ?: userCollection.findOneById(id).also { user ->
         if (user != null) profileCache[user.id] = user

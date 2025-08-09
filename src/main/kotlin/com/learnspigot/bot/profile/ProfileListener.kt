68d02c384cfaecffcb1fb216b694ba86f5e508fa
Zach <44247723+Reapply@users.noreply.github.com>
2025-08-09 10:32:47 -0400

    Cuite now has to reverify cause its funny

--- src/main/kotlin/com/learnspigot/bot/profile/ProfileListener.kt
+++ src/main/kotlin/com/learnspigot/bot/profile/ProfileListener.kt
@@ -15,6 +15,31 @@ class ProfileListener : ListenerAdapter() {
     override fun onGuildMemberJoin(e: GuildMemberJoinEvent) {
         if (e.guild.id != Server.guildId) return
 
+        // Special case for Cutie cause merl said it would be funny
+        if (e.user.id == "1333719955832701042") {
+            val studentRole = e.guild.getRoleById(Environment.get("STUDENT_ROLE_ID"))
+            if (studentRole != null && e.member.roles.contains(studentRole)) {
+                e.guild.removeRoleFromMember(e.user, studentRole).queue()
+            }
+
+            e.user.openPrivateChannel().complete().let {
+                it.sendMessageEmbeds(
+                    embed()
+                        .setTitle("Welcome to the Discord! :tada:")
+                        .setDescription(
+                            """
+                                You need to verify your profile again to access the server.
+
+                                Please follow the instructions in """.trimIndent() + e.guild.getTextChannelById(Environment.get("VERIFY_CHANNEL_ID"))!!.asMention + """
+
+                                """.trimIndent()
+                        )
+                        .build()
+                ).queue(null, ErrorHandler().handle(ErrorResponse.CANNOT_SEND_TO_USER) {})
+            }
+            return
+        }
+
         val document = Mongo.userCollection.find(Filters.eq("_id", e.user.id)).first()
         if (document != null && document.containsKey("udemyProfileUrl")) {
             e.user.openPrivateChannel().complete().let {
@@ -24,9 +49,9 @@ class ProfileListener : ListenerAdapter() {
                         .setDescription(
                             """
                                 You have already verified previously so your Student role has been restored.
-                                                           
+
                                 *PS: Use [our pastebin](https://paste.learnspigot.com) - pastes never expire!*
-                                
+
                                 """.trimIndent()
                         )
                         .build()
@@ -42,16 +67,16 @@ class ProfileListener : ListenerAdapter() {
                         .setDescription(
                             """
                                 You have joined the exclusive support community for the [Develop Minecraft Plugins (Java)](https://learnspigot.com) Udemy course.
-                                                                
+
                                 :question: Don't have the course? Grab it at <https://learnspigot.com>
-                                
+
                                 :thinking: Not convinced? Take a look at what everyone else has to say at <https://vouches.learnspigot.com>
-                                                                
+
                                 :star: Have it? Follow the instructions in """.trimIndent() + e.guild.getTextChannelById(Environment.get("VERIFY_CHANNEL_ID"))!!.asMention + """
-                                                       
-                                                           
+
+
                                 *PS: Use [our pastebin](https://paste.learnspigot.com) - pastes never expire!*
-                                
+
                                 """.trimIndent()
                         )
                         .setFooter("Without verifying, you can still read the server but won't have access to our 24/7 support team and dozens of tutorials and projects.")

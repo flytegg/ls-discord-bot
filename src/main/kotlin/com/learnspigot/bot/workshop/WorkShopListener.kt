840661a83372f5a3d2ac9e477ab4ab324dff74ac
Kolja <contact@kolja.lu>
2025-12-29 10:53:17 +0100

    users are able to have 2 threads now

--- src/main/kotlin/com/learnspigot/bot/workshop/WorkShopListener.kt
+++ src/main/kotlin/com/learnspigot/bot/workshop/WorkShopListener.kt
@@ -1,6 +1,7 @@
 package com.learnspigot.bot.workshop
 
 import com.learnspigot.bot.Server
+import com.learnspigot.bot.util.embed
 import net.dv8tion.jda.api.entities.channel.ChannelType
 import net.dv8tion.jda.api.events.channel.ChannelCreateEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
@@ -11,14 +12,23 @@ class WorkShopListener: ListenerAdapter() {
         val thread = event.channel.asThreadChannel()
         if (thread.parentChannel.id != Server.workshopChannel.id) return
 
-        val existing = thread.parentChannel.threadChannels.filter {
+        val threads = (thread.parentChannel.threadChannels + thread.parentChannel.retrieveArchivedPublicThreadChannels()).filter {
             it.owner == thread.owner && it.id != thread.id
-        }
+        }.distinct()
 
-        if (existing.isNotEmpty()) {
+        if (threads.size >= 2) {
             event.channel.delete().queue()
             thread.owner!!.user.openPrivateChannel().queue {
-                it.sendMessage("You cannot create a channel as you already have one!\n${existing[existing.size - 1].asMention}").queue()
+                it.sendMessageEmbeds(
+                    embed()
+                        .setAuthor("You cannot create more than two workshop threads!")
+                        .setTitle("Please delete one of the following to create a new one:")
+                        .setDescription("""
+                            ${threads[threads.size - 1].asMention}
+                            ${threads[threads.size - 2].asMention}
+                            """.trimIndent())
+                        .build()
+                ).queue()
             }
         }
     }

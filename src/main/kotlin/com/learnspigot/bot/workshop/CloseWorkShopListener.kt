5c4ba8ae0be9c82327c8d90d00c6062126a0f66d
MLGPenguin <baww123@gmail.com>
2026-01-14 23:48:11 +1000

    fix error when clicking buttons outside of help part 2

--- src/main/kotlin/com/learnspigot/bot/workshop/CloseWorkShopListener.kt
+++ src/main/kotlin/com/learnspigot/bot/workshop/CloseWorkShopListener.kt
@@ -5,12 +5,14 @@ import com.learnspigot.bot.Server
 import com.learnspigot.bot.Server.isManager
 import com.learnspigot.bot.util.closeAndLock
 import com.learnspigot.bot.util.embed
+import net.dv8tion.jda.api.entities.channel.ChannelType
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
 class CloseWorkShopListener : ListenerAdapter() {
 
     override fun onButtonInteraction(event: ButtonInteractionEvent) {
+        if (event.channel.type != ChannelType.GUILD_PUBLIC_THREAD) return
         val channel = event.channel.asThreadChannel()
         if (channel.parentChannel.id != Server.CHANNEL_WORKSHOP.id) return
         if (!event.componentId.endsWith("-close-button") && !event.componentId.startsWith(channel.id)) return

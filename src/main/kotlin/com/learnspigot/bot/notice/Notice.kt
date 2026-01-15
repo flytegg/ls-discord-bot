c330be5cec975e91324a19de36e6a7629c67879b
Merlijn <lijnbelt13@gmail.com>
2026-01-15 11:49:38 +0100

    feat(otice): add embeds

--- src/main/kotlin/com/learnspigot/bot/notice/Notice.kt
+++ src/main/kotlin/com/learnspigot/bot/notice/Notice.kt
@@ -6,10 +6,26 @@ import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction
 
 enum class Notice(val reply: ReplyCallbackAction.(targetUserId: Long) -> Unit, val description: String, val helpPostOnly: Boolean) {
     CLOSE({ targetUserId ->
-        setContent("<@$targetUserId> if your issue has been resolved, please run /close and _only_ select **people who helped**.")
+        val closeId = Server.GUILD.retrieveCommands().complete().firstOrNull { it.name == "close" }?.id
+        addEmbeds(embed()
+            .setTitle("Issue Fixed?")
+            .setDescription(" ")
+            .addField(
+                "Looks like your post issue is fixed",
+                "Use ${if (closeId == null) "/close" else "</close:$closeId>"} and select the helpers.",
+                false
+            )
+            .build()
+        )
+        setContent("<@$targetUserId> - Have you resolved your issue?")
     }, "Reminds someone to close the post.", true),
     HELP({ targetUserId ->
-        setContent("<@$targetUserId> if you need any help, please make a post in ${Server.CHANNEL_HELP.jumpUrl}.")
+        addEmbeds(embed()
+            .setTitle("Looking for assistance?")
+            .setDescription("Please make a post in ${Server.CHANNEL_HELP.jumpUrl} so we can help.")
+            .build()
+        )
+        setContent("<@$targetUserId> - Stuck on an issue?")
     }, "Informs people to use the help channel.", false),
     PING({ targetUserId ->
         val closeId = Server.GUILD.retrieveCommands().complete().firstOrNull { it.name == "close" }?.id

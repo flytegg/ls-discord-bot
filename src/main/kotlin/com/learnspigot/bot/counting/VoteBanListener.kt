3140c98f59038735c8245e823e39fca590dc2f47
Merlijn <lijnbelt13@gmail.com>
2026-01-16 12:23:17 +0100

    ssshhhhhhh

--- src/main/kotlin/com/learnspigot/bot/counting/VoteBanListener.kt
+++ src/main/kotlin/com/learnspigot/bot/counting/VoteBanListener.kt
@@ -18,7 +18,7 @@ class VoteBanListener : ListenerAdapter() {
             if (message.embeds.isEmpty()) return@queue
             if (message.getEmojiReactionCount(Server.EMOJI_UPVOTE) <= Server.VOTE_COUNTING_BAN_AMOUNT) return@queue
             val userId = message.embeds[0].description?.substringAfter("<@")?.substringBefore(">")?.toLongOrNull() ?: return@queue
-            Server.GUILD.retrieveMemberById(userId).queue { member -> Server.GUILD.addRoleToMember(member, Server.COUNTING_BANNED_ROLE) }
+            Server.GUILD.retrieveMemberById(userId).queue { member -> Server.GUILD.addRoleToMember(member, Server.COUNTING_BANNED_ROLE).queue() }
             message.editMessageEmbeds(
                 embed().setDescription("<@$userId> has been banned from counting.").build()
             ).queue { message.clearReactions().queue() }

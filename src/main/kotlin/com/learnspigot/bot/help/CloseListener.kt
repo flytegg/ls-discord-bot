3036c664067a9a5a285272f48fe0bb0e13f1d691
SuperPenguin <72254337+MLGPenguin@users.noreply.github.com>
2026-06-13 23:17:24 +1000

    Help: Send message in support when thread is closed manually to confirm it was intentional.

--- src/main/kotlin/com/learnspigot/bot/help/CloseListener.kt
+++ src/main/kotlin/com/learnspigot/bot/help/CloseListener.kt
@@ -6,11 +6,16 @@ import com.learnspigot.bot.Server.isManager
 import com.learnspigot.bot.Server.isStudent
 import com.learnspigot.bot.util.closeAndLock
 import com.learnspigot.bot.util.embed
+import com.learnspigot.bot.util.isChannel
 import com.learnspigot.bot.util.owns
 import com.learnspigot.bot.util.replyEphemeral
+import net.dv8tion.jda.api.components.actionrow.ActionRow
+import net.dv8tion.jda.api.components.buttons.Button
 import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.entities.channel.ChannelType
+import net.dv8tion.jda.api.entities.channel.concrete.ForumChannel
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel
+import net.dv8tion.jda.api.events.channel.update.ChannelUpdateArchivedEvent
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
@@ -20,6 +25,8 @@ class CloseListener : ListenerAdapter() {
     private inline val profileRegistry get() = Registry.PROFILES
 
     private fun Member.hasClosePermission(channel: ThreadChannel): Boolean = owns(channel) || isManager
+
+    private val postsBeingClosed = mutableSetOf<Long>()
 
     override fun onStringSelectInteraction(event: StringSelectInteractionEvent) {
         if (event.componentId != event.channel.id + "-contributor-selector") return
@@ -37,6 +44,11 @@ class CloseListener : ListenerAdapter() {
     }
 
     override fun onButtonInteraction(event: ButtonInteractionEvent) {
+        // Handle support "confirm close" button press.
+        if (event.componentId == "confirm-no-contributors") {
+            return event.message.delete().queue()
+        }
+
         if (event.channel.type != ChannelType.GUILD_PUBLIC_THREAD) return
         val channel = event.channel.asThreadChannel()
         if (channel.parentChannel.id != Server.CHANNEL_HELP.id && channel.parentChannel.id != Server.CHANNEL_CODE_REVIEW.id) return
@@ -48,6 +60,8 @@ class CloseListener : ListenerAdapter() {
         }
 
         event.editButton(event.button.asDisabled()).complete()
+
+        postsBeingClosed.add(channel.idLong)
 
         val contributors = profileRegistry.contributorSelectorCache[event.channel.id] ?: mutableListOf()
 
@@ -87,6 +101,22 @@ class CloseListener : ListenerAdapter() {
             .build()).complete()
 
         channel.closeAndLock()
+    }
+
+    // Listen for help posts being closed. Verify they are closed intentionally.
+    override fun onChannelUpdateArchived(e: ChannelUpdateArchivedEvent) {
+        if (e.channelType != ChannelType.GUILD_PUBLIC_THREAD) return
+        if (e.newValue != true) return
+        val channel = e.channel.asThreadChannel().takeIf { Server.CHANNEL_HELP.isChannel(it.parentChannel) } ?: return
+
+        if (!postsBeingClosed.remove(channel.idLong)) {
+            // Post has not been closed with /close
+            Server.CHANNEL_SUPPORT.sendMessageEmbeds(
+                embed().setTitle("Thread Closed Manually").setDescription("${channel.asMention} has been closed manually. Please confirm this is intentional.").build()
+            ).addComponents(
+                ActionRow.of(Button.success("confirm-no-contributors", "Confirm"))
+            ).queue()
+        }
     }
 
 }
